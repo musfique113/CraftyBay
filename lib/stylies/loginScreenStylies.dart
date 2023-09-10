31504cd584b1cce5b805b5d6fbc78873c57c1822
musfique113 <musfique113@gmail.com>
2023-09-10 16:45:05 +0600

    cleaned up the previous project

--- lib/stylies/loginScreenStylies.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ecommerceyt/appColors/appColors.dart';
-import 'package:flutter/material.dart';
-
-class LoginScreenStylies{
-  static const TextStyle forgotPasswordStylies=TextStyle(
-    color: AppColors.baseDarkPinkColor,
-    fontSize: 18
-  );
-}
