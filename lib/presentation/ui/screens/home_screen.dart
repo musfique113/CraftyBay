da149bc6937f564d45a4c731ee610bd6613f2725
musfique113 <musfique113@gmail.com>
2023-09-23 01:13:08 +0600

    make the product card as a resualbe compont and also added the special and new section

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:CraftyBay/presentation/utilities/app_colors.dart';
 import 'package:CraftyBay/presentation/utilities/custom_widgets/categories_cards.dart';
 import 'package:CraftyBay/presentation/utilities/custom_widgets/circular_icon_button_app_bar.dart';
 import 'package:CraftyBay/presentation/utilities/custom_widgets/home_screen_widgets/carousel_slider_home_screen.dart';
+import 'package:CraftyBay/presentation/utilities/custom_widgets/home_screen_widgets/products_card_widgets.dart';
 import 'package:CraftyBay/presentation/utilities/custom_widgets/home_screen_widgets/section_title_home_screen.dart';
 import 'package:CraftyBay/presentation/utilities/resources_path.dart';
 import 'package:flutter/material.dart';
@@ -80,81 +81,35 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               SectionTitle(title: "Popular", onTap: () {}),
-              Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(7)),
-                child: SizedBox(
-                  width: 150,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        decoration: BoxDecoration(
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(7),
-                                topRight: Radius.circular(7)),
-                            color: AppColors.primaryColor.withOpacity(0.1),
-                            image: const DecorationImage(
-                                image: AssetImage(ImageAssets.nikeShoePNG))),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(10, 2, 8, 0),
-                        child: Text(
-                          "Nike Shoe dsfdsGF ASDGFEW SDFWEgsasdS",
-                          maxLines: 1,
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black,
-                              overflow: TextOverflow.ellipsis),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(10, 0, 8, 2),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "\$1000",
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: AppColors.primaryColor,
-                                  overflow: TextOverflow.ellipsis),
-                            ),
-                            Wrap(crossAxisAlignment: WrapCrossAlignment.center, children: [
-                              Icon(
-                                Icons.star,
-                                size: 21,
-                                color: Colors.amber,
-                              ),
-                              Text(
-                                "4.5",
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.black,
-                                    overflow: TextOverflow.ellipsis),
-                              )
-                            ]),
-                            Card(
-                                color: AppColors.primaryColor,
-                                child: Padding(
-                                  padding: EdgeInsets.all(1.0),
-                                  child: Icon(
-                                    Icons.favorite_border,
-                                    size: 16,
-                                    color: Colors.white,
-                                  ),
-                                ))
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              SizedBox(
+                height: 165,
+                child: ListView.builder(
+                  itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                  return const ProductsCard();
+                }),
+              ),
+              SectionTitle(title: "Special", onTap: () {}),
+              SizedBox(
+                height: 165,
+                child: ListView.builder(
+                    itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return const ProductsCard();
+                    }),
+              ),
+              SectionTitle(title: "New", onTap: () {}),
+              SizedBox(
+                height: 165,
+                child: ListView.builder(
+                    itemCount: 20,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return const ProductsCard();
+                    }),
+              ),
             ],
           ),
         ),
